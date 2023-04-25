3d5d34e76ba98e8b957ce3d7d94553cdd76bdfdc
aswankotalh <74552374+aswankotalh@users.noreply.github.com>
2023-04-25 15:50:39 +0200

    start of a wonderful space shooter :]

--- src/js/game.js
+++ src/js/game.js
@@ -1,22 +1,42 @@
-import '../css/style.css'
-import { Actor, Engine, Vector } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
+import { Actor, Engine, Vector, Label, Color, Font } from "excalibur";
+import { Resources, ResourceLoader } from "./resources.js";
 
 export class Game extends Engine {
+  constructor() {
+    super({ width: 640, height: 480 });
+    this.start(ResourceLoader).then(() => this.startGame());
+  }
 
-    constructor() {
-        super({ width: 800, height: 600 })
-        this.start(ResourceLoader).then(() => this.startGame())
-    }
+  startGame() {
+    const sans = new Actor();
+    sans.graphics.use(Resources.Sans.toSprite());
+    sans.pos = new Vector(300, 200);
+    sans.scale = new Vector(0.2, 0.2);
+    this.add(sans);
 
-    startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
-    }
+
+    for (let i = 0; i < 100; i++) {
+        let fish = new Actor()
+        fish.graphics.use(Resources.Fish.toSprite());
+        // fish.pointer.useGraphicsBounds = true;
+        // fish.enableCapturePointer = true;
+        fish.pos = new Vector(Math.random() * 800 , Math.random() * 600);
+        fish.vel = new Vector(Math.random() * -10 , Math.random() * 10);
+        fish.scale = new Vector(0.1, 0.1);
+        this.add(fish);
+        fish.pointer.useGraphicsBounds = true;
+        fish.enableCapturePointer = true;
+        fish.on("pointerup", function (e) {
+            fish.kill();
+        });
+        }
+        const textbox = new Actor();
+        textbox.graphics.use(Resources.Text.toSprite());
+        textbox.pos = new Vector(320, 400);
+        textbox.scale = new Vector(1,1);
+        this.add(textbox);
+  }
+  
 }
 
-new Game()
+new Game();
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,13 @@
-import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import fishImage from '../images/fish.png'
+import { ImageSource, Sound, Resource, Loader } from "excalibur";
+import fishImage from "../images/bone.png";
+import sansImage from "../images/sans.png";
+import textImage from "../images/textbox.png";
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
-}
-const ResourceLoader = new Loader([Resources.Fish])
+  Fish: new ImageSource(fishImage),
+  Sans: new ImageSource(sansImage),
+  Text: new ImageSource(textImage),
+};
+const ResourceLoader = new Loader([Resources.Fish, Resources.Sans, Resources.Text]);
 
-export { Resources, ResourceLoader }
+export { Resources, ResourceLoader };
